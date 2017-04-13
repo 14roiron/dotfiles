b5c84a71d86cc49389afd05d2c148f3112f1547b
14roiron <yohann.roiron@mines-paristech.fr>
2017-04-13 17:41:41 +1100

    add install

---
+++ install.sh
@@ -0,0 +1,2 @@
+sh <(curl https://raw.githubusercontent.com/14roiron/spf13-vim/3.0/bootstrap.sh -L)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
