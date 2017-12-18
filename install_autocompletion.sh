91e71e040eed581b814667f14c53914a95dd015e
14roiron <yohann.roiron@mines-paristech.fr>
2017-12-18 19:02:51 +0000

    add auto comp install

---
+++ install_autocompletion.sh
@@ -0,0 +1,6 @@
+sudo apt-get install npm node clang
+sudo apt-get install build-essential cmake
+sudo apt-get install python-dev python3-dev
+curl https://sh.rustup.rs -sSf | sh
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --js-completer --rust-completer
