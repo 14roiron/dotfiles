629e7c6eecba58f4bd044db7f8c1003e843ca907
14roiron <14roiron@users.noreply.github.com>
2019-05-07 11:38:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions 
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-python2 -m pip install --user powerline-status
+python3 -m pip install --user wheel 
+python3 -m pip install --user powerline-status
 
 
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9kdd
